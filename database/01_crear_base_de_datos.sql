c105d7f56ee55b03f056a7a53aac4f8a203d0ccc
Pablo Axel Rojas Hernandez <pabloaxel.rojas@gmail.com>
2025-10-12 12:43:42 -0600

    chore(crear_base_de_datos): reordena parametros y agrega LOCALE_PROVIDER

--- database/01_crear_base_de_datos.sql
+++ database/01_crear_base_de_datos.sql
@@ -1,9 +1,11 @@
 -- Crear la base de datos
 CREATE DATABASE fca_auditorios
-    WITH ENCODING 'UTF8'
+    WITH
+    TEMPLATE template0
+    ENCODING 'UTF8'
     LC_COLLATE='Spanish_Spain.1252'
     LC_CTYPE='Spanish_Spain.1252'
-    TEMPLATE template0;
+    LOCALE_PROVIDER = 'libc';
 
 -- Agregar un comentario descriptivo
 COMMENT ON DATABASE fca_auditorios IS 'Base de datos del sistema de gestión de auditorios de la FCA';
